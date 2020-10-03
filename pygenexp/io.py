4200f4d71bce0efb60fff5ddc2314a2b69b5f489
IlyaBolkisev <69188079+IlyaBolkisev@users.noreply.github.com>
2020-10-03 16:37:55 +0500

    Added read_illumina Updated requirements

--- pygenexp/io.py
+++ pygenexp/io.py
@@ -1,12 +1,32 @@
 import pandas as pd
+import GEOparse
 import csv
 import os
 import re
 
 
-def read_illumnia(filename):
+def read_illumina(filename):
     df = pd.read_csv(filename, sep="\t")
     return df
+
+
+def read_illumina(filename, GSE="GSE40561"):
+    df = pd.read_csv(filename, sep="\t")
+    df.columns = ["Probe_Id"] + list(df.columns[1:])
+    cols = list(filter(lambda x: not x.startswith("Detect"), df.columns))
+    df = df[cols]
+    res_df = pd.DataFrame()
+    res_df["Probe_Id"] = df["Probe_Id"]
+    gse = GEOparse.get_GEO(GSE)
+    geneNames = list(gse.gsms.keys())
+
+    for gene in geneNames:
+        new_df = pd.DataFrame()
+        new_df["Probe_Id"] = gse.gsms[gene].table["ID_REF"]
+        new_df[gene] = gse.gsms[gene].table["VALUE"]
+        res_df = pd.merge(res_df, new_df)
+
+    return res_df
 
 
 def read_ngd(filename):
