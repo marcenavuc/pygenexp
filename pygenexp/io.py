9d4629696773dbb8301c876a2e31816d3b09e999
IlyaBolkisev <69188079+IlyaBolkisev@users.noreply.github.com>
2020-10-08 19:23:08 +0500

    Updated read_xys

--- pygenexp/io.py
+++ pygenexp/io.py
@@ -33,24 +33,18 @@ def read_ngd(filename):
     pass
 
 
-def read_xys(xys_files, ndf_file, sep="\t"):
-    assert ndf_file.endswith('.ndf'), "You must specify the path to the .ndf file"
-    assert os.path.exists(ndf_file), ".ndf file does not exist"
+def read_xys(xys_files, sep="\t"):
     for xys_file in xys_files:
         assert xys_file.endswith('.xys'), "You must specify the path to the .xys files"
         assert os.path.exists(xys_file), "One of the .xys files does not exist"
     new_df = pd.DataFrame()
-    ndf_df = pd.read_csv(ndf_file, sep=sep)
-
-    assert ("SEQ_ID" in ndf_df), ".ndf file must contain 'SEQ_ID' column"
-    new_df["id"] = ndf_df["SEQ_ID"]
 
     for xys in xys_files:
         xys_df = pd.read_csv(xys, sep=sep, skiprows=1)  # TO DO: add param
         assert ("SIGNAL" in xys_df), ".xys file must contain 'SIGNAL' column"
         new_df[os.path.basename(xys)] = xys_df['SIGNAL']
 
-    return new_df.dropna()
+    return new_df
 
 
 def read_bgx(filename):
